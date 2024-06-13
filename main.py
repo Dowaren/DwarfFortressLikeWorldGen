6e231c169ad0b8bdbed1a426cb2d7646459e5cc0
Toomadzaki <110834832+Dowaren@users.noreply.github.com>
2024-06-13 22:56:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ offsetz = 0
 def start(offsetz,offsety,offsetx):
     map = mapgeneration.mapgen()
     update(map, offsetz,offsety,offsetx)
+    
 def update(map, offsetz,offsety,offsetx):
     while True:
         fragment.render(map, offsetz, offsetx, offsety)
